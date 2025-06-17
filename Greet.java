49dcfff7d499435b0ab42bbb34fc202968cc3207
Esha-Pankaj <esharockspankaj@gmail.com>
2025-06-17 13:50:56 +0530

    Update Greet.java

--- Greet.java
+++ Greet.java
@@ -1,5 +1,5 @@
 public class Greet{
   public static void main(String[]args){
-       System.out.println("Hello Good Morning" + args[0]);
+       System.out.println("Hello Good Morning " + args[0]);
         }
  }
