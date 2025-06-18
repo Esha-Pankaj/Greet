5dbdf13ad29e3c6c0f8821935749d4373488d898
Esha-Pankaj <esharockspankaj@gmail.com>
2025-06-18 12:36:06 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,9 +1,7 @@
-import java.util.*;
 public class Sum{
    public static void main(String[]args){
-   Scanner sc=new Scanner(System.in);
-   int a=sc.nextInt();
-   int b=sc.nextInt();
+   int a=Integer.parseInt(args[0]);
+   int b=Integer.parseInt(args[1]);
        int sum=0;
        sum=a+b;
    System.out.println(sum);
